4887fe65f298c4d4bf2354cb300efa9528cb7dd7
tivi791 <stevenlenz791@gmail.com>
2025-06-15 20:14:09 -0400

    Create AdminPanel.js

---
+++ src/components/AdminPanel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AdminPanel() {
+  return (
+    <div>
+      <h2>Panel de Administración</h2>
+      <p>Solo los administradores pueden ver esto.</p>
+      {/* Aquí podrás mostrar brackets, resultados, lista de equipos, etc. */}
+    </div>
+  );
+}
+
+export default AdminPanel;
