5ab247db33ba2d7b72c8208553a56d945b17b6ff
tivi791 <stevenlenz791@gmail.com>
2025-06-15 20:15:50 -0400

    Create LoginRegistro.js

---
+++ src/components/LoginRegistro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LoginRegistro() {
+  return (
+    <div>
+      <h2>Iniciar sesión o registrarse</h2>
+      <p>Este es el lugar para que los jugadores entren a la plataforma.</p>
+      {/* Aquí luego pondremos los formularios de login y registro */}
+    </div>
+  );
+}
+
+export default LoginRegistro;
